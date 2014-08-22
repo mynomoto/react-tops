2ab17426d9743b41f70b236229555c784f235208
mynomoto <mynomoto@gmail.com>
2014-08-22 14:42:04 -0300

    Working example

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject react-tops "0.1.0-SNAPSHOT"
+  :description "FIXME: write this!"
+  :url "http://example.com/FIXME"
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [ring/ring "1.3.0"]
+                 [compojure "1.1.8"]
+                 [ring/ring-json "0.3.1"]]
+
+  :source-paths ["src/clj"])
---
+++ resources/public/js/main.js
@@ -0,0 +1,143 @@
+/** @jsx React.DOM */
+
+var InputWord = React.createClass({
+  handleSubmit: function() {
+    var word = this.refs.word.getDOMNode().value.trim();
+    if (!word) {
+      return false;
+    }
+    this.props.onWordSubmit({word: word});
+    this.refs.word.getDOMNode().value = '';
+    return false;
+  },
+  render: function() {
+    return (
+      <div className="input-group">
+        <input className="form-control" ref="word" type="text"> </input>
+        <span className="input-group-btn">
+          <button className="btn btn-primary" onClick={this.handleSubmit}>
+            Submit
+          </button>
+        </span>
+      </div>
+    );
+  }
+});
+
+var WordView = React.createClass({
+  render: function() {
+    var classString = "list-group-item";
+    if ("local" === this.props.origin && !this.props.valid && !this.props.invalid) {
+      classString += ' list-group-item-warning';
+    }
+    if (this.props.valid) {
+      classString += ' list-group-item-success';
+    }
+    if (this.props.invalid) {
+      classString += ' list-group-item-danger invalid';
+    }
+    return (
+      <li className={classString}>
+        {this.props.children}
+      </li>
+    );
+  }
+});
+
+var WordList = React.createClass({
+  render: function() {
+    var wordNodes = this.props.data.map(function (word) {
+      return (
+        <WordView origin={word.origin} valid={word.valid} invalid={word.invalid}>
+          {word.word}
+        </WordView>
+      );
+    });
+    return (
+      <ul className="list-group">
+        {wordNodes}
+      </ul>
+    );
+  }
+});
+
+var TopsComponent = React.createClass({
+  loadWordsFromServer: function() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      success: function(data) {
+        var ns = this.state.data.slice(0,9);
+        ns.unshift({word: data, origin: "server"});
+        this.setState({data: ns});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  },
+  handleWordSubmit: function(word) {
+    var ns = this.state.data.slice(0,9);
+    ns.unshift({word: word.word, origin: "local"});
+    this.setState({data: ns});
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'PUT',
+      contentType: 'application/json',
+      data: JSON.stringify(word),
+      processData: false,
+      success: function(data) {
+        var us = this.state.data.map(function (word) {
+          if (word.word == data.valid) {
+            word.valid = true;
+            return word;
+          }
+          return word;
+        });
+        this.setState({data: us});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        var us = this.state.data.map(function (word) {
+          if (word.word == xhr.responseJSON.invalid) {
+            word.invalid = true;
+            return word;
+          }
+          return word;
+        });
+        this.setState({data: us});
+      }.bind(this)
+    });
+  },
+  getInitialState: function() {
+    return {data: []};
+  },
+  componentDidMount: function() {
+    this.loadWordsFromServer();
+    setInterval(this.loadWordsFromServer, this.props.pollInterval);
+  },
+  render: function() {
+    return (
+      <div className="row">
+        <div className="col-lg-4 col-md-5 col-sm-6">
+          <h1> React Tops </h1>
+          <InputWord onWordSubmit={this.handleWordSubmit}/>
+          <WordList data={this.state.data}/>
+        </div>
+      </div>
+    );
+  }
+});
+
+var data = [
+  {word: "blabla", origin: "server"},
+  {word: "blibli", origin: "server"},
+  {word: "abcdef", origin: "local"},
+  {word: "abcdef", origin: "local", valid: true},
+  {word: "abcabcabc", origin: "local", invalid: true}
+];
+
+React.renderComponent(
+  <TopsComponent url="word" pollInterval={1000} />,
+  document.getElementById('tops')
+);
